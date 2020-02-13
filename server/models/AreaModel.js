4d774958316f08f0a90dec215c8e8ec693e564d1
Natalie-Gornicki <natalie.gornicki@gmail.com>
2020-02-13 13:07:47 +0100

    area model

--- server/models/AreaModel.js
+++ server/models/AreaModel.js
@@ -1,52 +1,44 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const todoSchema = {
-//     name: {
-//         type: String,
-//         require: true
-//     },
-//     time: {
-//         type: Number,
-//         require: true
-//     },
-//     difficulty: {
-//         type: String, 
-//         require: true
-//     }
-// }
-
-
-// const areaSchema = {
-//     id: {
-//         type: String,
-//         require: true,
-//         unique: true
-//     },
-//     name: {
-//         type: String,
-//         require: true
-//         // unique: true
-//     },
-//     priority: {
-//         type: String
-//     },
-//     date: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     todos: [
-//         todoSchema
-//     ]
-// }
+const todoSchema = {
+    todoName: {
+        type: String,
+        require: true
+    },
+    parts: {
+        type: Number
+    },
+    partName: {
+        type: String
+    },
+    time: {
+        type: Number,
+        require: true
+    },
+    difficulty: {
+        type: String,
+        require: true
+    }
+}
 
 const areaSchema = new Schema({
     areaTitle: {
         type: String
-    }
+    },
+    priority: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    todos: [
+        todoSchema
+    ]
 })
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     id: {
         type: String,
         required: true,
@@ -68,5 +60,5 @@ const userSchema = new mongoose.Schema ({
     areas: [areaSchema]
 })
 
-const AreaModel = mongoose.model('user', userSchema );
+const AreaModel = mongoose.model('user', userSchema);
 module.exports = AreaModel;
