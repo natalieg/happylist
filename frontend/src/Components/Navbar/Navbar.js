3ff38bf2c358c50c5289ec9fcbcd64c39344d452
Natalie <natalie.gornicki@gmail.com>
2020-02-17 20:23:06 +0100

    styling navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
     return (
-        <div>
+        <div className="navbar">
             <ul>
                 <li>
                     <a onClick={props.click} href="#">{props.name}</a>
