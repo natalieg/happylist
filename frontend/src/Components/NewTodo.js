c5e094a8923f55960ce61faf5ac624c1932c6fb0
Ojehomon1 <periju@web.de>
2020-02-18 10:18:19 +0100

    todoForm

--- frontend/src/Components/NewTodo.js
+++ frontend/src/Components/NewTodo.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function NewTodo() {
-    return (
-        <div>
-            <h1>ABC</h1>
-        </div>
-    )
+export default class NewTodo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>ABC</h1>
+                <form action="">
+                    <label htmlFor="todoName">todoname</label>
+                    <input type="text" name="todoName"/>
+                    <label htmlFor="parts">parts</label>
+                    <input type="text" name="parts"/>
+                    <label htmlFor="partName">partname</label>
+                    <input type="text" name="partName"/>
+                    <label htmlFor="time">time</label>
+                    <input type="text" name="time"/>
+                    <label htmlFor="difficulty">difficulty</label>
+                    <input type="text" name="difficulty"/>
+
+                </form>
+            </div>
+        )
+    }
 }
-
-export default NewTodo
